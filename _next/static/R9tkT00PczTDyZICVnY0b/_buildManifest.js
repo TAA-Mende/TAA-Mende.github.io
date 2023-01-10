bf57df2f7bb7fad34b1f78b3311eadbd2a2ab775
Sebastien-Ahkrin <Sebastien-Ahkrin@users.noreply.github.com>
2023-01-10 11:48:40 +0000

    deploy: a23b23f4f63cd4af397d9621fa119a5096c82fee

---
+++ _next/static/R9tkT00PczTDyZICVnY0b/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b6194bbe58d5c45e.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
