f76103bd75d38e0085c7366f7827bca5f8ed5c4e
Sebastien-Ahkrin <sebastien48criquet@gmail.com>
2023-01-10 12:47:13 +0100

    chore: install typescript

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import { AppProps } from "next/app";
+import Head from "next/head";
+import React from "react";
+
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>TAA - Mende</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
+}
